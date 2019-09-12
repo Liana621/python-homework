9e9b1872ecc7ab69a8f5a317a8a2eb865471262f
Liana Pilarte <lianaplrt89@gmail.com>
2019-09-12 19:19:21 -0400

    methods of reading that work

--- pybank/main.py
+++ pybank/main.py
@@ -1,3 +1,4 @@
+
 # Import the pathlib and csv library
 from pathlib import Path
 import csv
@@ -37,4 +38,53 @@ import pandas as pd
 df = pd.read_csv (r'C:\Users\Owner\Desktop\Trilogy\CU-NYC-FIN-PT-08-2019-U-C\02-Homework\02-Python\Instructions\PyBank\Resources\budget_data.csv')
 print (df)
       
+      
+#NOW WE ARE GETTING SOMEWHERE***************************************************
+import pandas as pd
+from pathlib import Path
+
+#set path
+csvpath = Path('budget_data.csv')
+      
+
+#Read & see
+budgetfile = pd.read_csv(csvpath)
+budgetfile.head()      
+      
+#Rename 2 make easier to type& View
+budgetfile = budgetfile.rename(columns={
+   "Date":"date",
+   "Profit/Losses":"profit_losses"})
+budgetfile.head()
+
+#Make Calculations on CSV!!!!!
+budgetfile['profit_losses'].max()
+
+#The total number of months included in the dataset
+budgetfile['date'].count()
+
+#Net Value of Profit Losses Coloum
+budgetfile['profit_losses'].sum()
+      
+#Mean of the above
+budgetfile['profit_losses'].mean()
+      
+#Changes to the PNL
+budgetfile['profit_losses'].sum()
+budgetfile['pnl_changes'] = budgetfile['profit_losses']-budgetfile['profit_losses'].shift()
+      
+coloum = (budgetfile['profit_losses']-budgetfile['profit_losses'].shift())
+
+coloum.mean()
+      
+
+      
+
+      
+
+      
+      
+
+      
+
       
